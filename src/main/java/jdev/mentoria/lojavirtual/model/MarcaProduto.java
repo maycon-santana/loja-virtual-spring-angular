2b1ade6ca868b04f20fd7dde00ae473c686c9017
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-05-13 17:48:57 -0300

    Criando a classe de MarcaProduto

--- src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
@@ -1,2 +1,26 @@
-package jdev.mentoria.lojavirtual.model;public class MarcaProduto {
+package jdev.mentoria.lojavirtual.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+
+    private String nomeDesc;
+
 }
