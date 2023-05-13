6da13117092af3c6bcda23c9d7877467b8a5f6cb
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-05-13 18:44:58 -0300

    Criando a classe de CategoriaProduto

--- src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -11,14 +11,14 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
     private Long id;
 
     @Column(name = "nome_descricao", nullable = false)
