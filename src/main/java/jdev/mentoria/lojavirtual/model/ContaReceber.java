6ec13a94075b568f8c1f405bc1501f09816cdffb
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-05-17 19:58:25 -0300

    Implementando a classe ContaReceber

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/ContaReceber.java
@@ -0,0 +1,46 @@
+package jdev.mentoria.lojavirtual.model;
+
+import jdev.mentoria.lojavirtual.enums.StatusContaReceber;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "conta_receber")
+@SequenceGenerator(name = "seq_conta_receber", sequenceName = "seq_conta_receber", allocationSize = 1, initialValue = 1)
+public class ContaReceber implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_receber")
+    private Long id;
+
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    private StatusContaReceber status;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtVencimento;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtPagamento;
+
+    private BigDecimal valorTotal;
+
+    private BigDecimal valorDesconto;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+}
