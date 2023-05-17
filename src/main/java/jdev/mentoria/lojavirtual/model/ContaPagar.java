7623d0130c1136337efdbcbad5bc2ff4b3156894
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-05-17 20:13:59 -0300

    Classe ContaPagar

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/ContaPagar.java
@@ -0,0 +1,50 @@
+package jdev.mentoria.lojavirtual.model;
+
+import jdev.mentoria.lojavirtual.enums.StatusContaPagar;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+    private Long id;
+
+    private String descricao;
+
+    private BigDecimal valorTotal;
+
+    private BigDecimal valorDesconto;
+
+    @Enumerated(EnumType.STRING)
+    private StatusContaPagar status;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtVencimento;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtPagamento;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_forn_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
+    private Pessoa pessoa_fornecedor;
+
+}
